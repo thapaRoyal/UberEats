b983f957f570f7a2a6f8656a51bdd659d093d83b
thapaRoyal <thaparoyal17@gmail.com>
2022-07-13 13:57:45 +0545

    Restaurant item

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import RestaurantItem from './src/components/RestaurantItem';
+import restaurants from './assets/data/restaurants.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* restaurant item */}
-      <View style={styles.restaurantContainer}>
-        <Image
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg',
-          }}
-          style={styles.image}
-        />
-        <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
-        <Text style={styles.subTitle}>Rs. 399.00 | 15-30 minutes</Text>
-      </View>
+      <RestaurantItem restaurant={restaurants[0]} />
+      <RestaurantItem restaurant={restaurants[1]} />
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -26,21 +22,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 10,
-  },
-  restaurantContainer: {
-    width: '100%',
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 5 / 3,
-    marginBottom: 5,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginVertical: 5,
-  },
-  subTitle: {
-    color: 'grey',
   },
 });
